ff9cf4e1751665b8966c9571161b79b22e2e1a19
samochodowy434-png <samochodowy434@gmail.com>
2026-06-02 17:04:18 +0200

    Update server.js

--- server.js
+++ server.js
@@ -12,13 +12,45 @@ io.on("connection", (socket) => {
 
     players[socket.id] = {
         x: 100,
-        y: 100
+        y: 100,
+        hp: 100,
+        team: Math.random() > 0.5 ? "red" : "blue"
     };
 
     socket.emit("init", players);
 
     socket.on("move", (data) => {
-        players[socket.id] = data;
+        if(players[socket.id]){
+            players[socket.id].x = data.x;
+            players[socket.id].y = data.y;
+        }
+        io.emit("players", players);
+    });
+
+    socket.on("shoot", (data) => {
+
+        for(let id in players){
+
+            if(id === socket.id) continue;
+
+            let p = players[id];
+
+            let dx = p.x - data.x;
+            let dy = p.y - data.y;
+
+            let dist = Math.sqrt(dx*dx + dy*dy);
+
+            if(dist < 50){
+                p.hp -= 20;
+            }
+
+            if(p.hp <= 0){
+                p.hp = 100;
+                p.x = 100;
+                p.y = 100;
+            }
+        }
+
         io.emit("players", players);
     });
 
