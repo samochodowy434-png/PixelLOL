1b3be910c91b31c1cef0f4b4038dec2cacb11453
samochodowy434-png <samochodowy434@gmail.com>
2026-06-02 18:37:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,34 +7,60 @@ app.use(express.static("public"));
 
 let players = {};
 
-io.on("connection", (socket) => {
+io.on("connection",(socket)=>{
 
 players[socket.id] = {
-x: 0,
-y: 2,
-z: 0,
-rotY: 0
+x: 200,
+y: 200,
+hp: 100,
+kills: 0
 };
 
-socket.on("move", (data) => {
-if (!players[socket.id]) return;
+socket.on("move",(data)=>{
+if(!players[socket.id]) return;
 
 players[socket.id].x = data.x;
 players[socket.id].y = data.y;
-players[socket.id].z = data.z;
-players[socket.id].rotY = data.rotY;
 });
 
-setInterval(() => {
-io.emit("players", players);
-}, 50);
+socket.on("shoot",(data)=>{
 
-socket.on("disconnect", () => {
+for(let id in players){
+if(id === socket.id) continue;
+
+let p = players[id];
+
+let dx = p.x - data.x;
+let dy = p.y - data.y;
+
+let dist = Math.sqrt(dx*dx + dy*dy);
+
+if(dist < 40){
+p.hp -= 25;
+
+if(p.hp <= 0){
+p.hp = 100;
+p.x = 200;
+p.y = 200;
+
+players[socket.id].kills++;
+}
+}
+}
+
+io.emit("players",players);
+});
+
+socket.on("disconnect",()=>{
 delete players[socket.id];
 });
 
+setInterval(()=>{
+io.emit("players",players);
+},50);
+
 });
 
-http.listen(3000, () => {
-console.log("Server działa na 3000");
+http.listen(3000,()=>{
+console.log("2D MIASTO działa");
 });
