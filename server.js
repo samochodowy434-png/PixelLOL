845c10135b7e27933ef75b7b7e75129445f2b2d3
samochodowy434-png <samochodowy434@gmail.com>
2026-06-02 18:13:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ app.use(express.static("public"));
 let players = {};
 
 let blocks = [
-{ x:0, y:0, z:0, type:0 },
-{ x:2, y:0, z:0, type:1 },
-{ x:4, y:0, z:0, type:2 }
+  {x:0,y:0,z:0,type:0},
+  {x:2,y:0,z:0,type:1},
+  {x:4,y:0,z:0,type:2}
 ];
 
 io.on("connection",(socket)=>{
@@ -18,42 +18,44 @@ io.on("connection",(socket)=>{
 players[socket.id] = {
 x:0,
 y:2,
-z:0
+z:0,
+rotY:0
 };
 
-socket.emit("init",{
-id:socket.id,
-blocks,
-players
+socket.emit("init",{id:socket.id,players,blocks});
+
+// MOVE
+socket.on("move",(p)=>{
+if(!players[socket.id]) return;
+
+players[socket.id].x = p.x;
+players[socket.id].y = p.y;
+players[socket.id].z = p.z;
+players[socket.id].rotY = p.rotY;
 });
 
-socket.on("move",(data)=>{
-let p = players[socket.id];
-if(!p) return;
-
-p.x = data.x;
-p.y = data.y;
-p.z = data.z;
-});
-
+// ADD BLOCK
 socket.on("addBlock",(b)=>{
 blocks.push(b);
-io.emit("worldUpdate",blocks);
+io.emit("world",blocks);
 });
 
+// REMOVE BLOCK
 socket.on("removeBlock",(index)=>{
+if(blocks[index]){
 blocks.splice(index,1);
-io.emit("worldUpdate",blocks);
+io.emit("world",blocks);
+}
 });
+
+// UPDATE PLAYERS
+setInterval(()=>{
+io.emit("players",players);
+},50);
 
 socket.on("disconnect",()=>{
 delete players[socket.id];
-io.emit("playersUpdate",players);
 });
-
-setInterval(()=>{
-io.emit("playersUpdate",players);
-},1000/20);
 
 });
 
