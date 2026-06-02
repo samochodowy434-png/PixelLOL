518d230efcbd55a5a97cf2ccfda42a3894ce2c4d
samochodowy434-png <samochodowy434@gmail.com>
2026-06-02 17:01:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-const server = require("http").createServer(app);
+const http = require("http").createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(http);
 
 app.use(express.static("public"));
 
@@ -10,27 +10,19 @@ let players = {};
 
 io.on("connection", (socket) => {
 
-    players[socket.id] = {
-        x: 100,
-        y: 100,
-        team: "red"
-    };
+players[socket.id] = { x: 100, y: 100 };
 
-    socket.emit("init", players);
+socket.emit("init", players);
 
-    socket.on("move", (data) => {
-        if (players[socket.id]) {
-            players[socket.id].x = data.x;
-            players[socket.id].y = data.y;
-        }
+socket.on("move", (data) => {
+players[socket.id] = data;
+io.emit("players", players);
+});
 
-        io.emit("players", players);
-    });
-
-    socket.on("disconnect", () => {
-        delete players[socket.id];
-    });
+socket.on("disconnect", () => {
+delete players[socket.id];
+});
 
 });
 
-server.listen(process.env.PORT || 3000);
+http.listen(process.env.PORT || 3000);
