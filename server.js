63cadbb874b9a956ab06c6d86f0fc0ac91915f32
samochodowy434-png <samochodowy434@gmail.com>
2026-06-02 18:26:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,54 +7,34 @@ app.use(express.static("public"));
 
 let players = {};
 
-let world = [];
-
-// STARTOWA PŁASKA MAPA (ZIEMIA)
-for(let x=-20;x<20;x++){
-for(let z=-20;z<20;z++){
-world.push({x, y:0, z, type:2});
-}
-}
-
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
 
 players[socket.id] = {
-x:0,
-y:2,
-z:0,
-rotY:0
+x: 0,
+y: 2,
+z: 0,
+rotY: 0
 };
 
-socket.emit("init",{id:socket.id, world});
+socket.on("move", (data) => {
+if (!players[socket.id]) return;
 
-socket.on("move",(p)=>{
-let pl = players[socket.id];
-if(!pl) return;
-
-pl.x = p.x;
-pl.y = p.y;
-pl.z = p.z;
-pl.rotY = p.rotY;
+players[socket.id].x = data.x;
+players[socket.id].y = data.y;
+players[socket.id].z = data.z;
+players[socket.id].rotY = data.rotY;
 });
 
-socket.on("addBlock",(b)=>{
-world.push(b);
-io.emit("world",world);
-});
+setInterval(() => {
+io.emit("players", players);
+}, 50);
 
-socket.on("removeBlock",(index)=>{
-world.splice(index,1);
-io.emit("world",world);
-});
-
-setInterval(()=>{
-io.emit("players",players);
-},50);
-
-socket.on("disconnect",()=>{
+socket.on("disconnect", () => {
 delete players[socket.id];
 });
 
 });
 
-http.listen(process.env.PORT || 3000);
+http.listen(3000, () => {
+console.log("Server działa na 3000");
+});
