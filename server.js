d537bcbc04f964c70aa4cf2c7d0cb6b8124cc623
samochodowy434-png <samochodowy434@gmail.com>
2026-06-02 17:11:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,59 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(http);
+const io = require("socket.io")(http);
 
 app.use(express.static("public"));
 
 let players = {};
-let bullets = [];
 
 io.on("connection", (socket) => {
 
     players[socket.id] = {
-        x: 200,
-        y: 200,
+        x: 300,
+        y: 300,
+        angle: 0,
         hp: 100,
         team: Math.random() > 0.5 ? "red" : "blue"
     };
 
-    socket.emit("init", { players, id: socket.id });
+    socket.emit("init", socket.id);
 
-    socket.on("move", (data) => {
+    socket.on("state", (data) => {
         if(players[socket.id]){
             players[socket.id].x = data.x;
             players[socket.id].y = data.y;
+            players[socket.id].angle = data.angle;
         }
     });
 
-    socket.on("shoot", (data) => {
+    socket.on("shoot", () => {
 
-        bullets.push({
-            x: data.x,
-            y: data.y,
-            dx: data.dx,
-            dy: data.dy,
-            owner: socket.id
-        });
+        let p1 = players[socket.id];
+        if(!p1) return;
 
+        for(let id in players){
+            if(id === socket.id) continue;
+
+            let p2 = players[id];
+
+            let dx = p2.x - p1.x;
+            let dy = p2.y - p1.y;
+
+            let dist = Math.sqrt(dx*dx + dy*dy);
+
+            if(dist < 80){
+                p2.hp -= 25;
+
+                if(p2.hp <= 0){
+                    p2.hp = 100;
+                    p2.x = 200;
+                    p2.y = 200;
+                }
+            }
+        }
+
+        io.emit("players", players);
     });
 
     socket.on("disconnect", () => {
@@ -44,42 +61,5 @@ io.on("connection", (socket) => {
     });
 
 });
-
-// 🔥 game loop serwera
-setInterval(()=>{
-
-    // bullet movement
-    for(let i = bullets.length - 1; i >= 0; i--){
-
-        let b = bullets[i];
-        b.x += b.dx;
-        b.y += b.dy;
-
-        for(let id in players){
-
-            if(id === b.owner) continue;
-
-            let p = players[id];
-
-            let dx = p.x - b.x;
-            let dy = p.y - b.y;
-
-            if(Math.sqrt(dx*dx + dy*dy) < 15){
-                p.hp -= 25;
-                bullets.splice(i,1);
-
-                if(p.hp <= 0){
-                    p.hp = 100;
-                    p.x = 200;
-                    p.y = 200;
-                }
-                break;
-            }
-        }
-    }
-
-    io.emit("state", {players, bullets});
-
-}, 1000/30);
 
 http.listen(process.env.PORT || 3000);
