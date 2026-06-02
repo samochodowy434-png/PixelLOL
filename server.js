858167e6048f3f8d46c24c6be5106c79ed131de1
samochodowy434-png <samochodowy434@gmail.com>
2026-06-02 17:06:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,51 +7,36 @@ const io = new Server(http);
 app.use(express.static("public"));
 
 let players = {};
+let bullets = [];
 
 io.on("connection", (socket) => {
 
     players[socket.id] = {
-        x: 100,
-        y: 100,
+        x: 200,
+        y: 200,
         hp: 100,
         team: Math.random() > 0.5 ? "red" : "blue"
     };
 
-    socket.emit("init", players);
+    socket.emit("init", { players, id: socket.id });
 
     socket.on("move", (data) => {
         if(players[socket.id]){
             players[socket.id].x = data.x;
             players[socket.id].y = data.y;
         }
-        io.emit("players", players);
     });
 
     socket.on("shoot", (data) => {
 
-        for(let id in players){
+        bullets.push({
+            x: data.x,
+            y: data.y,
+            dx: data.dx,
+            dy: data.dy,
+            owner: socket.id
+        });
 
-            if(id === socket.id) continue;
-
-            let p = players[id];
-
-            let dx = p.x - data.x;
-            let dy = p.y - data.y;
-
-            let dist = Math.sqrt(dx*dx + dy*dy);
-
-            if(dist < 50){
-                p.hp -= 20;
-            }
-
-            if(p.hp <= 0){
-                p.hp = 100;
-                p.x = 100;
-                p.y = 100;
-            }
-        }
-
-        io.emit("players", players);
     });
 
     socket.on("disconnect", () => {
@@ -59,5 +44,42 @@ io.on("connection", (socket) => {
     });
 
 });
+
+// 🔥 game loop serwera
+setInterval(()=>{
+
+    // bullet movement
+    for(let i = bullets.length - 1; i >= 0; i--){
+
+        let b = bullets[i];
+        b.x += b.dx;
+        b.y += b.dy;
+
+        for(let id in players){
+
+            if(id === b.owner) continue;
+
+            let p = players[id];
+
+            let dx = p.x - b.x;
+            let dy = p.y - b.y;
+
+            if(Math.sqrt(dx*dx + dy*dy) < 15){
+                p.hp -= 25;
+                bullets.splice(i,1);
+
+                if(p.hp <= 0){
+                    p.hp = 100;
+                    p.x = 200;
+                    p.y = 200;
+                }
+                break;
+            }
+        }
+    }
+
+    io.emit("state", {players, bullets});
+
+}, 1000/30);
 
 http.listen(process.env.PORT || 3000);
